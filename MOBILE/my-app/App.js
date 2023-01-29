5c71d82a9de3a82214c4f9a320e73b441847a2bc
c-alvares <cyrobrandaoalvares@gmail.com>
2023-01-28 23:04:39 -0300

    Pendente utilização de navigation no App.js

--- MOBILE/my-app/App.js
+++ MOBILE/my-app/App.js
@@ -1,41 +1,14 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
-import { View, Text, TextInput } from "react-native";
+import { View, Text } from "react-native";
 
 import styles from "./src/styles/style";
 
 // import ButtonFinish from "./src/components/ButtonFinish";
 // import ButtonCancel from "./src/components/ButtonCancel";
-import ButtonSubmit from "./src/components/ButtonSubmit";
+
 
 export default function App() {
-
-  // Recebimento dos dados inseridos nos inputs e cadastro de nova tarefa
-  const [description, setDescription] = useState([]);
-  const [time, setTime] = useState([]);
-
-  const submitTask = (value1, value2) => {
-    // console.log(data1, data2)
-    const data = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(
-        {
-          descricao: value1,
-          hora_inicio: value2
-        })
-    }
-    fetch("http://localhost:3000/criartarefa", data)
-      .then((response) => response.status)
-      .then((resp) => {
-        if (resp === 200) {
-          console.log("Pedido Cadastrado");
-        } else {
-          console.log(resp.status)
-        }
-      })
-  }
-
 
 
   // Consumo da API para importação dos pedidos a serem preparados
@@ -57,29 +30,12 @@ export default function App() {
       })
   }
 
-
-
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerText}>Todas as tarefas</Text>
       </View>
-      <View>
-        <TextInput style={styles.subscribeInput}
-          onChangeText={(value) => { setDescription(value) }}
-          placeholder="Descrição"
-          placeholderTextColor="#E4DDB3">
-        </TextInput>
-        <TextInput style={styles.subscribeInput}
-          onChangeText={(value) => { setTime(value) }}
-          placeholder="Horário de Início"
-          placeholderTextColor="#E4DDB3">
-        </TextInput>
-        <ButtonSubmit
-          value="Cadastrar Nova Tarefa"
-          onPress={() => { submitTask(description, time) }}>
-        </ButtonSubmit>
-      </View>
+
       {task.map((toDo, index) => {
         return (
           <View style={styles.boxOrder} key={index}>
