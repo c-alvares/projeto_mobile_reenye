110e9aec55c1f8835f6131424f2f811a94eb65cf
c-alvares <cyrobrandaoalvares@gmail.com>
2023-01-31 08:48:38 -0300

    Rotas create e read estabelecidas

--- BACKEND/SRC/ROUTES/tasks.routes.js
+++ BACKEND/SRC/ROUTES/tasks.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const Tasks = require('../CONTROLLERS/tasks.controller');
+const PrismaStatus = require('../CONTROLLERS/prisma.status.controller');
+const PrismaTask = require('../CONTROLLERS/prisma.task.controller')
 
 router.post("/criartarefa", Tasks.createTask);
 router.get("/listartarefas", Tasks.listAllTasks);
@@ -10,5 +12,13 @@ router.get("/tarefasfinalizadas", Tasks.listFinishedTasks);
 router.get("/tarefascanceladas", Tasks.listCanceledTasks);
 router.put("/finalizartarefa", Tasks.toFinishTask);
 router.put("/cancelartarefas", Tasks.toCancelTask);
+
+
+
+router.post("/creatstatus", PrismaStatus.create);
+router.get("/readstatus", PrismaStatus.read);
+
+router.post("/createtask", PrismaTask.create);
+router.get("/readtask", PrismaTask.read);
 
 module.exports = router;
